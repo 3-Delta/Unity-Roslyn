0d884ee848263eb40c84632baaf7a8fe62be39eb
kaclok <kaclok@users.noreply.github.com>
2021-07-06 02:39:56 +0800

    还原上次测试

--- Framework/Main.cs
+++ Framework/Main.cs
@@ -20,29 +20,6 @@ namespace Framework {
         }
     }
 
-    public class Test {
-        public int a;
-        public float fl;
-        public List<string> strList = new List<string>() { "qwe", "rty" };
-        public List<FormInt2> formList = new List<FormInt2>() { new FormInt2(6, 6), new FormInt2(9, 9) };
-
-        public Test() {
-            a = 3;
-            fl = 1f;
-        }
-
-        public static int StaticAdd(int b) {
-            return 2 + b;
-        }
-
-        public int InstanceAdd(int b) {
-            //Vector2 v2 = new Vector2(1, 4);
-            //Console.WriteLine(v2);
-
-            return a + b;
-        }
-    }
-
     internal class Program {
         // https://blog.csdn.net/WPwalter/article/details/80545207
         private static void Main(string[] args) {
@@ -78,11 +55,6 @@ namespace Framework {
                     string jsonPath = Environment.CurrentDirectory + "Json.json";
                     string json = JsonConvert.SerializeObject(instance, Formatting.Indented);
                     File.WriteAllText(jsonPath, json);
-                    Console.WriteLine("Json = " + json);
-
-                    json = File.ReadAllText(jsonPath);
-                    var hotfixJsonObj = JsonConvert.DeserializeObject<Test>(json);
-                    json = JsonConvert.SerializeObject(hotfixJsonObj, Formatting.Indented);
                     Console.WriteLine("Json = " + json);
                 }
                 else {
